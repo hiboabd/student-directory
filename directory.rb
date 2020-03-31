38b68622d88c149501336c7687470caa0ae8630c
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-03-31 16:44:41 +0000

    Amend code structure

--- directory.rb
+++ directory.rb
@@ -1,34 +1,5 @@
-# Let's put all the students in an array
-students = [ 
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex Delarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november},
-]
-def print_header
-    puts "The students of Villains Academy"
-    puts "------------"
-end 
-
-def print(students)
-    students.each do |student| 
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end 
-end 
-
-def print_footer(students)
-    puts "Overall we have #{students.count} great students"
-end 
-
 def input_students
-    puts "Please eneter the names of the students"
+    puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
     students = []
@@ -44,6 +15,21 @@ def input_students
     end 
     # return the array of students 
     students
+end 
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "------------"
+end 
+
+def print(students)
+    students.each do |student| 
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end 
+end 
+
+def print_footer(students)
+    puts "Overall we have #{students.count} great students"
 end 
 
 # nothing will happen till we call the methods
