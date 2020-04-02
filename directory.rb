b8af4701ae4496e6ac482e163930a3f84cd76f6e
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-04-02 14:25:45 +0000

    Add functionality to load student file data

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # get the first name
-    name = gets.chomp
+    name = STDIN.gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
         # add the student hash to the array
@@ -71,21 +71,34 @@ def save_students
     file.close
 end 
 
-def load_students
-    file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+    file = File.open(filename, "r")
     file.readlines.each do |line|
-        name, cohort = line.chomp.split(',')
+    name, cohort = line.chomp.split(',')
         @students << {name: name, cohort: cohort.to_sym}
     end 
     file.close
 end 
 
+def try_load_students
+    filename = ARGV.first # first argument from the command line
+    return if filename.nil? # get out of method if it isnt given
+    if File.exists?(filename) # if it exists
+        load_students(filename)
+        puts "Loaded #{@students.count} from #{filename}"
+    else #if it doesn't exist
+        puts "Sorry, #{filename} doesn't exist."
+        exit # quit the program
+    end 
+end 
+
 def interactive_menu
     loop do
         print_menu
-        process(gets.chomp)
+        process(STDIN.gets.chomp)
     end 
 end
 
 # nothing will happen till we call the methods
+try_load_students
 interactive_menu
