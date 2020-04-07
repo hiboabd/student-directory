51540f393837efa5f5efac94a0b120eee2296d9f
hiboabd <51047911+hiboabd@users.noreply.github.com>
2020-04-07 09:46:21 +0000

    Step 14 exercises

--- directory.rb
+++ directory.rb
@@ -1,48 +1,20 @@
+require 'csv'
 @students = []
-
-def input_students
-    puts "Please enter the names of the students"
-    puts "To finish, just hit return twice"
-    # get the first name
-    name = STDIN.gets.chomp
-    # while the name is not empty, repeat this code
-    while !name.empty? do
-        # add the student hash to the array
-        @students << {name: name, cohort: :november}
-        puts "Now we have #{@students.count} students"
-        # get another name from the user 
-        name = gets.chomp
-    end 
-end 
-
-def print_header
-    puts "The students of Villains Academy"
-    puts "------------"
-end 
-
-def print_students_list
-     @students.each do |student| 
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end 
-end 
-
-def print_footer
-    puts "Overall we have #{@students.count} great students"
-end 
 
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
-    puts "3. Save the list to students.csv"
+    puts "3. Save the list of students"
     puts "4. Load the list from students.csv"
     puts "9. Exit"
 end 
 
-def show_students
-    print_header
-    print_students_list
-    print_footer
-end 
+def interactive_menu
+    loop do
+        print_menu
+        process(STDIN.gets.chomp)
+    end 
+end
 
 def process(selection)
     case selection
@@ -61,44 +33,74 @@ def process(selection)
     end 
 end 
 
-def save_students
-    file = File.open("students.csv", "w")
-    @students.each do |student|
-        student_data = [student[:name], student[:cohort]]
-        csv_line = student_data.join(",")
-        file.puts csv_line
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    name = STDIN.gets.chomp # get the first name
+    while !name.empty? do # while the name is not empty, repeat this code
+        # add the student hash to the array
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
+        # get another name from the user 
+        name = STDIN.gets.chomp
     end 
-    file.close
+    puts "Inputting students action successful"
 end 
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+    puts "Showing students action successful"
+end 
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "------------"
+end 
+
+def print_students_list
+     @students.each do |student| 
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end 
+end 
+
+def print_footer
+    puts "Overall we have #{@students.count} great students"
+end 
+
+def save_students
+    puts "What would you like to name the file?"
+    filename = gets.chomp
+    CSV.open("#{filename}.csv", "wb") do |csv|
+        @students.each do |hash|
+            csv << hash.values
+        end 
+    end 
+    puts "Saving students to #{filename} successful."
+end
 
 def load_students(filename = "students.csv")
-    file = File.open(filename, "r")
-    file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-        @students << {name: name, cohort: cohort.to_sym}
-    end 
-    file.close
+    CSV.foreach("#{filename}") do |row|
+        @students << {name: row[0], cohort: row[1].to_sym}
+    end
+    puts @students
 end 
 
-def try_load_students
+def start_load_students
     filename = ARGV.first # first argument from the command line
-    return if filename.nil? # get out of method if it isnt given
-    if File.exists?(filename) # if it exists
+    if filename.nil?
+        load_students()
+        puts "Student data loaded from students.csv by default"
+    elsif File.exists?(filename) # if it exists
         load_students(filename)
         puts "Loaded #{@students.count} from #{filename}"
     else #if it doesn't exist
         puts "Sorry, #{filename} doesn't exist."
         exit # quit the program
-    end 
+    end
 end 
 
-def interactive_menu
-    loop do
-        print_menu
-        process(STDIN.gets.chomp)
-    end 
-end
-
 # nothing will happen till we call the methods
-try_load_students
+start_load_students
 interactive_menu
